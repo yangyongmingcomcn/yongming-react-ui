d8fc79f2d65806476f045c6b541d19b49f1a7d5a
yang yongming <YONGMING.YANG@HAND-CHINA.COM>
2020-04-23 22:38:05 +0800

    feat: 新增路由跳转

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,29 +1,73 @@
-import React from "react";
+import React, { useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  NavLink,
+} from "react-router-dom";
+
 import "./index.less";
+
+function Index() {
+  useEffect(() => {
+    console.log(`userEffect=>Index`);
+    return () => {
+      console.log(`userEffect=>Return Index`);
+    };
+  }, []);
+  return <h2>yangyongming.com</h2>;
+}
+
+function List() {
+  useEffect(() => {
+    console.log(`userEffect=>List`);
+    return () => {
+      console.log(`userEffect=>Return List`);
+    };
+  }, []);
+  return <h2>List Page</h2>;
+}
 
 function App() {
   return (
     <div className="container">
       <header className="navbar">
-        <a href="#">
-          <span className="site-name">Yongming UI</span>
-        </a>
-        <div className="navbar-right">
-          <div className="search-box">
-            <input />
+        <Router>
+          <NavLink to="/">
+            <span className="site-name">Youngming UI</span>
+          </NavLink>
+          <div className="navbar-right">
+            <div className="search-box">
+              <input />
+            </div>
+            <div className="nav-link">
+              <div className="nav-item">
+                <NavLink exact to="/" activeClassName="activeClass">
+                  主页
+                </NavLink>
+              </div>
+              <div className="nav-item">
+                <NavLink to="/list" activeClassName="activeClass">
+                  文档
+                </NavLink>
+              </div>
+              <div className="nav-item">
+                <a
+                  target="_blank"
+                  className="external"
+                  rel="noopener noreferrer"
+                  href="https://github.com/yangyongmingcomcn/yongming-react-ui"
+                >
+                  GitHub
+                </a>
+              </div>
+            </div>
           </div>
-          <div className="nav-link">
-            <div className="nav-item aa">
-              <a href="#">主页</a>
-            </div>
-            <div className="nav-item">
-              <a href="#">文档</a>
-            </div>
-            <div className="nav-item external">
-              <a href="https://github.com/yangyongmingcomcn/yongming-react-ui" target="_blank">GitHub</a>
-            </div>
-          </div>
-        </div>
+          <Switch>
+            <Route exact path="/" component={Index} />
+            <Route path="/list" component={List} />
+          </Switch>
+        </Router>
       </header>
     </div>
   );
