56e464bad8e6a51049f99d0e2f23deec81b1c21d
yang yongming <YONGMING.YANG@HAND-CHINA.COM>
2020-04-23 14:31:58 +0800

    feat: 头组件开发

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,29 @@
-import React from 'react';
-import './index.css';
+import React from "react";
+import "./index.less";
 
 function App() {
   return (
     <div className="container">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <header className="navbar">
+        <a href="#">
+          <span className="site-name">Yongming UI</span>
         </a>
+        <div className="navbar-right">
+          <div className="search-box">
+            <input />
+          </div>
+          <div className="nav-link">
+            <div className="nav-item aa">
+              <a href="#">主页</a>
+            </div>
+            <div className="nav-item">
+              <a href="#">文档</a>
+            </div>
+            <div className="nav-item external">
+              <a href="https://github.com/yangyongmingcomcn/yongming-react-ui" target="_blank">GitHub</a>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
